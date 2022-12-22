e3b3db1bdf019ade8a28586fa46be6cd278ee54e
alikafy <kafy_ali@yahoo.com>
2022-12-23 00:34:46 +0330

    add sigmoid function

--- network/activation_functions.py
+++ network/activation_functions.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from abc import ABC
 
+from numpy import exp
+
 
 class ActivationFunction(ABC):
     def function(self, x):
@@ -17,3 +19,11 @@ class Linear(ActivationFunction):
 
     def derivative(self, x):
         return 1
+
+
+class Sigmoid(ActivationFunction):
+    def function(self, x):
+        return 1.0 / (1.0 + exp(-x))
+
+    def derivative(self, x):
+        return x * (1.0 - x)
